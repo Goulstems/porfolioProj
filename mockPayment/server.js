eb6a07e9ca0af2b23820970f44d98284b9a91278
Josh Morvant <sk84life4evr@Gmail.com>
2025-09-13 01:26:58 -0700

    untracked files

--- mockPayment/server.js
+++ mockPayment/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('/var/www/portfolioProj/GeneralTutoring'));
+app.use(express.static('/var/www/portfolioProj/'));
 
 
 app.get('/api/config', (req, res) => {
